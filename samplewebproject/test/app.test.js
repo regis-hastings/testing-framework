38ed5f89cb4337aa4db94c22af6a6bb6d4720869
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-06-17 11:38:50 -0700

    fix delayed report bug

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const render = require('../../render');
 
 it('has a text input', async () => {
   const dom = await render('index.html');
@@ -6,4 +7,18 @@ it('has a text input', async () => {
   const input = dom.window.document.querySelector('input');
 
   assert(input);
+});
+
+it('shows a success message with a valid email', async () => {
+  const dom = await render('index.html');
+
+  const input = dom.window.document.querySelector('input');
+  input.value = 'regis@regis.com';
+  dom.window.document
+    .querySelector('form')
+    .dispatchEvent(new dom.window.Event('submit'));
+
+  const h1 = dom.window.document.querySelector('h1');
+
+  console.log('Contents of h1', h1.innerHTML);
 });
